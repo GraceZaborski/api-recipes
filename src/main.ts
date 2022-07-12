504d185f23fd76c40db6fa7b8233c3f8e7468a32
Mike Hughes <mike.hughes@beamery.com>
2022-07-12 21:22:50 +0100

    fix(core): use correct default port

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const PORT: number = Number(process.env.PORT) || 8080;
+const PORT: number = Number(process.env.PORT) || 8000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
