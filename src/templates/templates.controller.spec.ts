658f8bd58b12036f98ec10791604c331a7f124bc
Mike Hughes <mike.hughes@beamery.com>
2022-07-20 20:42:51 +0100

    test(templates): add happy path tests for templates controller

--- src/templates/templates.controller.spec.ts
+++ src/templates/templates.controller.spec.ts
@@ -1,9 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TemplatesController } from './templates.controller';
 import { TemplatesService } from './templates.service';
+import { Chance } from 'chance';
+
+const chance = new Chance();
 
 describe('TemplatesController', () => {
-  let controller: TemplatesController;
+  let templatesController: TemplatesController;
+  let templatesService: TemplatesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -22,10 +26,58 @@ describe('TemplatesController', () => {
       ],
     }).compile();
 
-    controller = module.get<TemplatesController>(TemplatesController);
+    templatesController = module.get<TemplatesController>(TemplatesController);
+    templatesService = module.get<TemplatesService>(TemplatesService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(templatesController).toBeDefined();
+  });
+
+  it('should be able to get all templates', async () => {
+    const filterQuery = {
+      limit: null,
+      offset: null,
+      search: null,
+    };
+
+    await templatesController.getAllTemplates(filterQuery);
+
+    expect(templatesService.findAll).toHaveBeenCalledWith(filterQuery);
+  });
+
+  it('should add the correct data when creating a template', async () => {
+    const newTemplate = {
+      title: chance.sentence(),
+      subject: chance.sentence(),
+      recipientVariables: [],
+      unlayer: {
+        json: {},
+        previewUrl: chance.url(),
+      },
+    };
+
+    const authData = {
+      userId: chance.guid(),
+      companyId: chance.guid(),
+    };
+
+    const mockDate = new Date();
+    const spy = jest
+      .spyOn(global, 'Date')
+      .mockImplementation(() => mockDate as unknown as string);
+
+    await templatesController.createTemplate(newTemplate, authData);
+
+    expect(templatesService.create).toHaveBeenCalledWith({
+      ...newTemplate,
+      createdBy: authData.userId,
+      companyId: authData.companyId,
+      createdAt: mockDate,
+      updatedAt: null,
+      updatedBy: null,
+    });
+
+    spy.mockRestore();
   });
 });
