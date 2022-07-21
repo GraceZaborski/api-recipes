75e58982856762b38a03dec1b4b5758ed81808f9
Mike Hughes <mike.hughes@beamery.com>
2022-07-21 13:17:32 +0100

    test: fix app e2e test for root path

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from '@cerbero/mod-auth';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { HeartbeatController } from './heartbeat/heartbeat.controller';
 import { LoggerModule } from './logger';
 import { TemplatesModule } from './templates/templates.module';
@@ -26,7 +24,7 @@ import configuration from './config/configuration';
     }),
     TemplatesModule,
   ],
-  controllers: [AppController, HeartbeatController],
-  providers: [AppService],
+  controllers: [HeartbeatController],
+  providers: [],
 })
 export class AppModule {}
