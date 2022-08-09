2a4ed86b6c7e2733e5d43620af5afca583bed5e6
Mike Hughes <mike.hughes@beamery.com>
2022-08-09 11:55:08 +0100

    fix: update schema and exception filter to use native mongo error

--- src/filters/mongoValidationException.filter.ts
+++ src/filters/mongoValidationException.filter.ts
@@ -1,9 +1,12 @@
 import { Catch, ConflictException, ExceptionFilter } from '@nestjs/common';
-import { ValidationError } from 'mongoose/lib/error/validation';
-
-@Catch(ValidationError)
+import { MongoServerError } from 'mongodb';
+@Catch(MongoServerError)
 export class MongoValidationExceptionFilter implements ExceptionFilter {
-  catch(exception: ValidationError) {
-    throw new ConflictException(exception.message);
+  catch(exception: MongoServerError) {
+    if (exception.code === 11000) {
+      throw new ConflictException(exception.message);
+    }
+
+    throw exception;
   }
 }
