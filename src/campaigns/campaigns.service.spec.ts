e83b942554f1d2abb72b15c101a8ba345e35f3e7
Mike Hughes <mike.hughes@beamery.com>
2022-09-15 11:57:45 +0100

    test: seed db service tests

--- src/campaigns/campaigns.service.spec.ts
+++ src/campaigns/campaigns.service.spec.ts
@@ -1,9 +1,12 @@
 import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Model } from 'mongoose';
 import { CampaignsService } from './campaigns.service';
+import { CampaignDocument } from './schemas/campaign.schema';
 
 describe('CampaignsService', () => {
   let service: CampaignsService;
+  let model: Model<CampaignDocument>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -35,9 +38,28 @@ describe('CampaignsService', () => {
     }).compile();
 
     service = module.get<CampaignsService>(CampaignsService);
+    model = module.get<Model<CampaignDocument>>(
+      getModelToken('Campaign', 'seed'),
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should be able to find one', async () => {
+    const campaign = {
+      id: '1',
+      name: 'test',
+      settings: {},
+    };
+
+    jest.spyOn(model, 'findOne').mockReturnValue({
+      lean: jest.fn().mockResolvedValueOnce(campaign),
+    } as any);
+
+    const result = await service.findOne('1');
+    expect(result).toEqual(campaign);
+    expect(model.findOne).toBeCalledWith({ id: '1' });
   });
 });
