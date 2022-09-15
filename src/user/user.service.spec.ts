a2458d8a28862eeb367bec1cb8c1a4facb89d652
Mike Hughes <mike.hughes@beamery.com>
2022-09-15 14:15:36 +0100

    test(user): user service tests

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -4,6 +4,7 @@ import { proto } from '@beamery/chimera-user-client';
 
 describe('UserService', () => {
   let service: UserService;
+  let userClient: proto.User;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,15 +12,45 @@ describe('UserService', () => {
         UserService,
         {
           provide: proto.User,
-          useValue: {},
+          useValue: {
+            getUser: jest.fn(),
+          },
         },
       ],
     }).compile();
 
     service = module.get<UserService>(UserService);
+    userClient = module.get<proto.User>(proto.User);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should be able to request a user', async () => {
+    const user = { id: 'userId', companyId: 'companyId' };
+    jest.spyOn(userClient, 'getUser').mockImplementation(
+      () =>
+        ({
+          user,
+        } as any),
+    );
+
+    const request = proto.GetUserRequest.fromObject({
+      authorization: {
+        userId: user.id,
+        companyId: user.companyId,
+      },
+      id: user.id,
+      company_id: user.companyId,
+    });
+
+    const result = await service.getUser({
+      id: user.id,
+      companyId: user.companyId,
+    });
+    expect(result).toEqual(user);
+
+    expect(userClient.getUser).toBeCalledWith(request);
   });
 });
